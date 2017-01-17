12ce3348334f95239a5f6797556e05e5a493cd50
Shekar Ramaswamy <shekarramaswamy@neils-macbook-air.local>
2017-01-17 08:06:13 -0800

    Initial Commit

---
+++ MinesweeperCommandLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MinesweeperCommandLine
+//
+//  Created by Shekar Ramaswamy on 1/17/17.
+//  Copyright © 2017 Shekar Ramaswamy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
