79b8bf6e1de2ffd3587d3ba12e74f4aa63ff2b57
Shekar Ramaswamy <shekarramaswamy@neils-macbook-air.local>
2017-01-22 12:23:51 -0800

    initial push

--- MinesweeperCommandLine/main.swift
+++ MinesweeperCommandLine/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+var board = Board(size: 4)
+board.setupBoard(testing: true)
+board.setupMine(row: 0, col: 0)
+board.setupValues()
+board.printBoard(printAll:true)
+print("\n")
+
+//
+//var x = board.handleClick(row: 2, col: 2)
+//board.printBoard(printAll:false)
+//
+//print("\n")
+//
+//board = Board(size: 8)
+//board.setupBoard(testing: true)
+//board.setupMine(row: 0, col: 0)
+//board.setupMine(row: 1, col: 1)
+//board.setupValues()
+//board.printBoard(printAll:true)
+//print("\n")
+//
+//x = board.handleClick(row: 2, col: 2)
+//board.printBoard(printAll:false)
+//
+//print("\n")
+
+//var board = Board(size: 8)
+//board.setupBoard(testing: false)
+//board.printBoard(printAll:true)
+//print("\n")
+//
+//var x = board.handleClick(row: 2, col: 2)
+//board.printBoard(printAll:false)
+
+
+
+
 
